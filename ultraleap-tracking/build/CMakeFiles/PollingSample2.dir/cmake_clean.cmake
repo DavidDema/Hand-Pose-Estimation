3b91b12b00743623319b9da1e31214b5a25caab3
davidDema <daviddema98@gmail.com>
2023-04-06 15:22:56 +0200

    added Ultraleap tracking

---
+++ ultraleap-tracking/build/CMakeFiles/PollingSample2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PollingSample2.dir/PollingSample2.c.o"
+  "CMakeFiles/PollingSample2.dir/PollingSample2.c.o.d"
+  "PollingSample2"
+  "PollingSample2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PollingSample2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
