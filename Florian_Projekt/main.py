f7ad12ea7525bedc1f890ae8ee6955e5fb3624f3
FlorianHofs <fhofstetter5@gmail.com>
2023-04-11 13:25:08 +0200

    compute world coordinates with solvePnP

--- Florian_Projekt/main.py
+++ Florian_Projekt/main.py
@@ -7,6 +7,7 @@ from helper_functions import *
 
 thumb_landmark_index = 4
 index_landmark_index = 8
+plotdata = True
 # Webcam
 cap = cv2.VideoCapture(0)
 # cap.set(3, 1280)
@@ -17,7 +18,9 @@ mtx, dist, aruco_params, aruco_dict = start_aruco()
 pixel_size = (mtx[0][0] + mtx[1][1]) / 2
 marker_size = 0.04
 pixel_distance = 50
-mp_hands = mp.solutions.hands.Hands()
+mp_hands = mp.solutions.hands.Hands(model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5)
 
 fig = plt.figure(figsize=(20, 20))
 ax = fig.add_subplot(projection='3d')
@@ -28,8 +31,40 @@ while True:
     results = mp_hands.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
     # Get the position of the thumb
+    world_points_total = []
     if results.multi_hand_landmarks:
+
         landmarks = results.multi_hand_landmarks[0]
+        #draw_hand(ax, landmark_array, "red","left", "o")
+        model_points = np.float32([[-l.x, -l.y, -l.z] for l in landmarks.landmark])
+        image_points = np.float32([[l.x * img.shape[1], l.y * img.shape[0]] for l in landmarks.landmark])
+        landmark_array = np.array([[landmark.x, landmark.y, landmark.z] for landmark in landmarks.landmark])
+        success, rvecs, tvecs, = cv2.solvePnP(
+            model_points,
+            image_points,
+            mtx,
+            dist,
+            flags=cv2.SOLVEPNP_SQPNP
+        )
+        transformation = np.eye(4)  # needs to 4x4 because you have to use homogeneous coordinates
+        transformation[0:3, 3] = tvecs.squeeze()
+        # the transformation consists only of the translation, because the rotation is accounted for in the model coordinates. Take a look at this (https://codepen.io/mediapipe/pen/RwGWYJw to see how the model coordinates behave - the hand rotates, but doesn't translate
+
+        # transform model coordinates into homogeneous coordinates
+        model_points_hom = np.concatenate((model_points, np.ones((21, 1))), axis=1)
+
+        # apply the transformation
+        world_points = model_points_hom.dot(np.linalg.inv(transformation).T)
+        #print(world_points[:,0:3])
+        world_points_total.append(world_points)
+        if plotdata:
+            ax.cla()
+            ax.set_xlim3d(-1.5, 1.5)
+            ax.set_ylim3d(-1.5, 1.5)
+            ax.set_zlim3d(-1.5, 0)
+            draw_hand(ax, world_points[:,0:3], 'red', 'left', 'o')
+            plt.pause(.001)
+        #print(np.array([landmarks.landmark])[:].x)
         thumb_x = landmarks.landmark[thumb_landmark_index].x * img.shape[1]
         thumb_y = landmarks.landmark[thumb_landmark_index].y * img.shape[0]
         thumb_z = landmarks.landmark[thumb_landmark_index].z
